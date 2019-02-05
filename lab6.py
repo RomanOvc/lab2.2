e426755ff4fe3c41349401d755604af7b3d7a69c
Roman <roman.ovcharov.97@mail.ru>
2019-02-05 14:10:31 +0300

    ldsfs

--- lab6.py
+++ lab6.py
@@ -1,87 +1,122 @@
-slovar1 = {'А': {'В': '^'}, 'Б': {'И': '@'}, 'В': {'О': ')'}, 'Г': {'А': '+'}, 'Д': {'Щ': '<'}, 'Е': {'П': '>'},
-           'Ж': {'К': 'Ɐ'}, 'З': {'Б': '♦'}, 'И': {'Ъ': '*'}, 'К': {' ': '♥'}, 'Л': {'Р': '♠'}, 'М': {'Т': '№'},
-           'Н': {'Ц': '#'}, 'О': {'.': '-'}, 'П': {'Ж': '='}, 'Р': {'Г': '('}, 'С': {'Л': '?'}, 'Т': {'Х': '%'},
-           'У': {'С': '⊗'}, 'Ф': {'Ь': '!'}, 'Х': {'Ч': '&'}, 'Ц': {'З': '®'}, 'Ч': {'М': 'Σ'}, 'Ш': {'У': '▽'},
-           'Щ': {'Д': 'Ⲩ'}, 'Ъ': {'Э': 'κ'}, 'Ы': {'Н': '⊕'}, 'Ь': {'Ю': '×'}, 'Э': {'Ы': 'ω'}, 'Ю': {'Ш': '$'},
-           'Я': {'Е': '⧍'}, ' ': {'Ф': '∞'}, '.': {'Я': '♣'}, }
+# slovar1 = {'А': {'В': '^'}, 'Б': {'И': '@'}, 'В': {'О': ')'}, 'Г': {'А': '+'}, 'Д': {'Щ': '<'}, 'Е': {'П': '>'},
+#            'Ж': {'К': 'Ɐ'}, 'З': {'Б': '♦'}, 'И': {'Ъ': '*'}, 'К': {' ': '♥'}, 'Л': {'Р': '♠'}, 'М': {'Т': '№'},
+#            'Н': {'Ц': '#'}, 'О': {'.': '-'}, 'П': {'Ж': '='}, 'Р': {'Г': '('}, 'С': {'Л': '?'}, 'Т': {'Х': '%'},
+#            'У': {'С': '⊗'}, 'Ф': {'Ь': '!'}, 'Х': {'Ч': '&'}, 'Ц': {'З': '®'}, 'Ч': {'М': 'Σ'}, 'Ш': {'У': '▽'},
+#            'Щ': {'Д': 'Ⲩ'}, 'Ъ': {'Э': 'κ'}, 'Ы': {'Н': '⊕'}, 'Ь': {'Ю': '×'}, 'Э': {'Ы': 'ω'}, 'Ю': {'Ш': '$'},
+#            'Я': {'Е': '⧍'}, ' ': {'Ф': '∞'}, '.': {'Я': '♣'}, }
+#
+#
+#
+# def sh_1_sh_2_zamena(slovo,slovar):
+#     str1 = ""
+#     for st in slovo:
+#         for key, val in dict.items(slovar):
+#             for key1, val1 in dict.items(val):
+#                 if ord(key1) == ord(st):
+#                     str1 += key
+#                 elif ord(val1) == ord(st):
+#                     str1 += key
+#     return str1
+#
+# def generate_key(word, key):
+#     gen_key = ""
+#     while len(word) > len(gen_key):
+#         for st in key:
+#             if len(word) > len(gen_key):
+#                 gen_key += st
+#     return gen_key
+#
+#
+# def hash_vijner(word, key, slovar):
+#     gen_key = generate_key(word, key)
+#     str1 = []
+#     for kk in gen_key:
+#             str1.append(slovar.index(kk))
+#     str2 = []
+#     for lo in word:
+#             str2.append(slovar.index(lo))
+#     x = ''
+#     for i in range(0, len(str2)):
+#         y = (str1[i] + str2[i]) % len(slovar)
+#         x += (slovar[y])
+#     return x
+#
+#
+# def de_hash_vijner(word, key, slovar):
+#     gen_key = generate_key(word, key)
+#     str1 = []
+#     for kk in gen_key:
+#         if kk in slovar:
+#             str1.append(slovar.index(kk))
+#     str2 = []
+#     for lo in word:
+#         if lo in slovar:
+#             str2.append(slovar.index(lo))
+#     x = ''
+#     for i in range(0, len(str2)):
+#         y = (str2[i] - str1[i] + len(slovar)) % len(slovar)
+#         x += (slovar[y])
+#     return x
 
 
+# def x_16(str):
+#     a = bin((int(str, 16)))
+#     return a
+#
+#
+# def x_2(str):
+#     a = bin((int(str, 2)))
+#
+#     return a
+#
+#
+# def x_2_in_x_16(str):
+#     a = int(str, 2)
+#     b = hex(a)
+#     return b[2:]
+#
+#
+# def koks(str1, str2):
+#     n1 = int(str1, 2)
+#     n2 = int(str2, 2)
+#     bit_or = n1 + n2
+#     kek = bin(bit_or)
+#
+#     return kek[2:]
+#
 
-def sh_1_sh_2_zamena(slovo,slovar):
-    str1 = ""
-    for st in slovo:
-        for key, val in dict.items(slovar):
-            for key1, val1 in dict.items(val):
-                if ord(key1) == ord(st):
-                    str1 += key
-                elif ord(val1) == ord(st):
-                    str1 += key
-    return str1
+#
+#
+#
+#
 
-def generate_key(word, key):
-    gen_key = ""
-    while len(word) > len(gen_key):
-        for st in key:
-            if len(word) > len(gen_key):
-                gen_key += st
-    return gen_key
+# if __name__ == "__main__":
+#     print("ЗАдание 1")
+#     str12 = "И.РЮУ.ЪФОБГНО"
+#     str123 = "▽*!(∞♦^№>#⊕"
+#     print(sh_1_sh_2_zamena(str12, slovar1))
+#     print(sh_1_sh_2_zamena(str123, slovar1))
+#     print()
+#     print()
+#     print("Шифр Вижнера.Зашифровал")
+#     slovar1 = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+#     slovar1_1 = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ_"
+#     print("Расшифровать слова с помощью ключа ОРЕХ")
+#     key1 = "ОРЕХ"
+#     word = "ШВМБУЖНЯ"
+#
+#     cryped = hash_vijner(word, key1, slovar1)
+#
+#     print(cryped)
+#
+#     decrypted = de_hash_vijner(word, key1, slovar1_1)
+#
+#     print(decrypted)
+#
+#     print("Задание 2")
+#     strok1 = "A5"
+#     strok2 = "01110111"
+#     xe = koks(x_16(strok1), x_2(strok2))
+#     print(x_2_in_x_16(xe))
 
-
-def hash_vijner(word, key, slovar):
-    gen_key = generate_key(word, key)
-    str1 = []
-    for kk in gen_key:
-            str1.append(slovar.index(kk))
-    str2 = []
-    for lo in word:
-            str2.append(slovar.index(lo))
-    x = ''
-    for i in range(0, len(str2)):
-        y = (str1[i] + str2[i]) % len(slovar)
-        x += (slovar[y])
-    return x
-
-
-def de_hash_vijner(word, key, slovar):
-    gen_key = generate_key(word, key)
-    str1 = []
-    for kk in gen_key:
-        if kk in slovar:
-            str1.append(slovar.index(kk))
-    str2 = []
-    for lo in word:
-        if lo in slovar:
-            str2.append(slovar.index(lo))
-    x = ''
-    for i in range(0, len(str2)):
-        y = (str2[i] - str1[i] + len(slovar)) % len(slovar)
-        x += (slovar[y])
-    return x
-
-
-
-
-
-if __name__ == "__main__":
-    print("ЗАдание 1")
-    str12 = "И.РЮУ.ЪФОБГНО"
-    str123 = "▽*!(∞♦^№>#⊕"
-    print(sh_1_sh_2_zamena(str12, slovar1))
-    print(sh_1_sh_2_zamena(str123,slovar1))
-    print()
-    print()
-    print("Шифр Вижнера.Зашифровал")
-    slovar1 = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
-    slovar1_1 = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ_"
-    print("Расшифровать слова с помощью ключа ОРЕХ")
-    key1 = "ОРЕХ"
-    word = "ШВМБУЖНЯ"
-
-    cryped = hash_vijner(word, key1, slovar1)
-
-    print(cryped)
-
-    decrypted = de_hash_vijner(word, key1, slovar1_1)
-
-    print(decrypted)
 
